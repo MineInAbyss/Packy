872378395e3fe48d8fb7c791ace69837b0582130
Boy <sivertpaulsen2@gmail.com>
2023-10-18 20:27:14 +0200

    make single-pack subPacks toggle only

--- src/main/kotlin/com/mineinabyss/packy/menus/picker/PackyMainMenu.kt
+++ src/main/kotlin/com/mineinabyss/packy/menus/picker/PackyMainMenu.kt
@@ -5,6 +5,7 @@ import com.mineinabyss.guiy.components.Item
 import com.mineinabyss.guiy.modifiers.Modifier
 import com.mineinabyss.guiy.modifiers.clickable
 import com.mineinabyss.idofront.items.editItemMeta
+import com.mineinabyss.idofront.messaging.broadcast
 import com.mineinabyss.idofront.messaging.logSuccess
 import com.mineinabyss.idofront.textcomponents.miniMsg
 import com.mineinabyss.idofront.textcomponents.serialize
@@ -22,8 +23,26 @@ fun PackyUIScope.PackyMenu() {
     packy.config.menu.subMenus.values.forEach { subMenu ->
         var packs by remember { mutableStateOf(subPackList[subMenu]!!.toMutableList()) }
 
-        when (subMenu.type) {
-            PackyConfig.SubMenuType.MENU -> Item(subMenu.button.toItemStack(), subMenu.modifiers.toModifier().clickable { nav.open(PackySubScreen(subMenu)) })
+        if (subMenu.packs.size == 1) {
+            val templateId = subMenu.packs.keys.first()
+            broadcast(templateId)
+            val template = packy.templates.find { it.id == templateId } ?: return
+
+            Item(subMenu.button.toItemStack(), subMenu.modifiers.toModifier().clickable {
+                // Return if the task returns null, meaning button was spammed whilst a set was currently generating
+                when {
+                    template !in player.packyData.enabledPackAddons -> PackPicker.addPack(player, templateId)
+                    else -> PackPicker.removePack(player, templateId)
+                } ?: return@clickable
+
+                packs = packs.toMutableList().apply { add(removeFirst()) }
+                hasChanged = true
+                nav.refresh()
+            })
+        } else when (subMenu.type) {
+            PackyConfig.SubMenuType.MENU -> Item(
+                subMenu.button.toItemStack(),
+                subMenu.modifiers.toModifier().clickable { nav.open(PackySubScreen(subMenu)) })
 
             PackyConfig.SubMenuType.CYCLING -> {
                 val (templateId, pack) = packs.first().first to packs[1].second
