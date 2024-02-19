40fe804e0f54826cdea8aac3780ffc74a6209e5a
Boy <sivertpaulsen2@gmail.com>
2024-02-19 14:14:55 +0100

    feat: send actionbar message whilst generating pack

--- src/main/kotlin/com/mineinabyss/packy/helpers/PackyServer.kt
+++ src/main/kotlin/com/mineinabyss/packy/helpers/PackyServer.kt
@@ -1,10 +1,17 @@
 package com.mineinabyss.packy.helpers
 
+import com.github.shynixn.mccoroutine.bukkit.launch
+import com.github.shynixn.mccoroutine.bukkit.ticks
+import com.mineinabyss.idofront.messaging.broadcast
 import com.mineinabyss.idofront.messaging.logError
 import com.mineinabyss.idofront.messaging.logSuccess
 import com.mineinabyss.idofront.textcomponents.miniMsg
 import com.mineinabyss.packy.components.packyData
 import com.mineinabyss.packy.config.packy
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
 import org.bukkit.entity.Player
 import team.unnamed.creative.serialize.minecraft.MinecraftResourcePackWriter
 import team.unnamed.creative.server.ResourcePackServer
@@ -18,13 +25,26 @@ object PackyServer {
 
     suspend fun sendPack(player: Player) {
         val templateIds = player.packyData.enabledPackIds
+
+        val actionBarJob = packy.plugin.launch { while (isActive) player.sendPackGeneratingActionBar() }
         val resourcePack = PackyGenerator.getOrCreateCachedPack(templateIds).await()
+        actionBarJob.cancel()
+        player.sendActionBar(Component.empty())
         player.setResourcePack(
             packy.config.server.publicUrl(resourcePack.hash(), templateIds),
             resourcePack.hash(),
             packy.config.force && !player.packyData.bypassForced,
             packy.config.prompt.miniMsg()
         )
+    }
+
+    private suspend fun Player.sendPackGeneratingActionBar() {
+        sendActionBar(Component.text("Generating ResourcePack.", NamedTextColor.RED))
+        delay(10.ticks)
+        sendActionBar(Component.text("Generating ResourcePack..", NamedTextColor.RED))
+        delay(10.ticks)
+        sendActionBar(Component.text("Generating ResourcePack...", NamedTextColor.RED))
+        delay(10.ticks)
     }
 
     fun startServer() {
